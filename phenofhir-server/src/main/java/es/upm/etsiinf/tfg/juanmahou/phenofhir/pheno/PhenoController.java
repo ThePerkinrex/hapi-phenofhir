d04ed0bc515c9b70ed230a5470d9e6f72d9816ae
ThePerkinrex <theperkinrex@gmail.com>
2025-06-01 18:15:50 +0200

    Add pheno endpoints

--- phenofhir-server/src/main/java/es/upm/etsiinf/tfg/juanmahou/phenofhir/pheno/PhenoController.java
+++ phenofhir-server/src/main/java/es/upm/etsiinf/tfg/juanmahou/phenofhir/pheno/PhenoController.java
@@ -2,6 +2,8 @@ package es.upm.etsiinf.tfg.juanmahou.phenofhir.pheno;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import es.upm.etsiinf.tfg.juanmahou.phenofhir.persistence.RepositoryProvider;
+import org.phenopackets.schema.v2.Cohort;
+import org.phenopackets.schema.v2.Family;
 import org.phenopackets.schema.v2.Phenopacket;
 import org.springframework.core.ResolvableType;
 import org.springframework.data.repository.CrudRepository;
@@ -19,10 +21,18 @@ import java.util.Optional;
 public class PhenoController {
     private final CrudRepository<entities.org.phenopackets.schema.v2.Phenopacket,
             entities.org.phenopackets.schema.v2.Phenopacket.Key> phenoRepo;
+    private final CrudRepository<entities.org.phenopackets.schema.v2.Family,
+            entities.org.phenopackets.schema.v2.Family.Key> familyRepo;
+    private final CrudRepository<entities.org.phenopackets.schema.v2.Cohort,
+            entities.org.phenopackets.schema.v2.Cohort.Key> cohortRepo;
 
     public PhenoController(RepositoryProvider repositoryProvider) {
         this.phenoRepo =
                 repositoryProvider.getCrudRepository(ResolvableType.forClass(entities.org.phenopackets.schema.v2.Phenopacket.class));
+        this.familyRepo =
+                repositoryProvider.getCrudRepository(ResolvableType.forClass(entities.org.phenopackets.schema.v2.Family.class));
+        this.cohortRepo =
+                repositoryProvider.getCrudRepository(ResolvableType.forClass(entities.org.phenopackets.schema.v2.Cohort.class));
     }
 
     @GetMapping(path = "/Phenopacket/{id}",
@@ -30,9 +40,33 @@ public class PhenoController {
                     "application/x-protobuf",      // ProtobufHttpMessageConverter
                     "application/json",            // ProtobufJsonFormatHttpMessageConverter
             })
-    public Phenopacket getPheno(@PathVariable("id") String id) throws InvalidProtocolBufferException {
+    public Phenopacket getPhenopacket(@PathVariable("id") String id) throws InvalidProtocolBufferException {
         Optional<entities.org.phenopackets.schema.v2.Phenopacket> pheno = phenoRepo.findById(new entities.org.phenopackets.schema.v2.Phenopacket.Key(id));
         if(pheno.isPresent()) return pheno.get().asPheno();
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Phenopacket with id '" + id + "' not found");
+    }
+
+
+
+    @GetMapping(path = "/Family/{id}",
+            produces = {
+                    "application/x-protobuf",      // ProtobufHttpMessageConverter
+                    "application/json",            // ProtobufJsonFormatHttpMessageConverter
+            })
+    public Family getFamily(@PathVariable("id") String id) throws InvalidProtocolBufferException {
+        Optional<entities.org.phenopackets.schema.v2.Family> pheno = familyRepo.findById(new entities.org.phenopackets.schema.v2.Family.Key(id));
+        if(pheno.isPresent()) return pheno.get().asPheno();
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Family with id '" + id + "' not found");
+    }
+
+    @GetMapping(path = "/Cohort/{id}",
+            produces = {
+                    "application/x-protobuf",      // ProtobufHttpMessageConverter
+                    "application/json",            // ProtobufJsonFormatHttpMessageConverter
+            })
+    public Cohort getCohort(@PathVariable("id") String id) throws InvalidProtocolBufferException {
+        Optional<entities.org.phenopackets.schema.v2.Cohort> pheno = cohortRepo.findById(new entities.org.phenopackets.schema.v2.Cohort.Key(id));
+        if(pheno.isPresent()) return pheno.get().asPheno();
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cohort with id '" + id + "' not found");
     }
 }
